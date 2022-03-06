ab67db55b37bf881c39a618a67f9eec038be6eb5
yoosion030 <rsy5487@naver.com>
2022-03-06 16:26:21 +0900

    다른 사람의 풀이

--- 약수의 개수와 덧셈.js
+++ 약수의 개수와 덧셈.js
@@ -1,17 +1,11 @@
 function solution(left, right) {
     let answer = 0;
 
-    // left ~ right까지 반복
-     for(let i = left; i<=right; i++) {
-         let cnt = 0;  
-        //1 ~ i까지 반복하여 약수 개수 판별
-        for(let j = 1; j <= i; j++) if(i % j === 0) cnt += 1;
-        // 약수 개수를 판별하여 더하거나 뺌
-        if(cnt % 2 == 0) answer += i;
-        else answer -= i;
-        cnt = 0;
+    for(let i = left; i <= right; i++) {
+        if(Number.isInteger(Math.sqrt(i))) answer -= i;
+        else answer += i;
     }
-    return answer;
+    console.log(answer)
 }
 
 solution(13, 17);
