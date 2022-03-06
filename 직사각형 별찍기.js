fbc92f02ff1987c7dc0e01bc952c9ad5dbcf365c
yoosion030 <rsy5487@naver.com>
2022-03-06 15:57:23 +0900

    다른 사람의 풀이

--- 직사각형 별찍기.js
+++ 직사각형 별찍기.js
@@ -3,12 +3,8 @@ process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
 
-    let result = "";
-    for(let i = 0; i<b; i++) {
-        for(let j = 0; j<a; j++) {
-            result += "*";
-        }
-        result +='\n'
+    const row = '*'.repeat(a);
+    for(let i = 0; i < b; i++) {
+        console.log(row);
     }
-    console.log(result)
 });
