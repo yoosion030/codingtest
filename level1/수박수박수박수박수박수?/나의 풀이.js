a7cf1a49f7cdf45a51e1e92f53f30905676fc6fa
yoosion030 <rsy5487@naver.com>
2022-07-31 00:09:27 +0900

    수박수박수박수박수박수?

---
+++ level1/수박수박수박수박수박수?/나의 풀이.js
@@ -0,0 +1,4 @@
+function solution(n) {
+  let answer = "수박";
+  return answer.repeat(n).substring(0, n);
+}
---
+++ level1/수박수박수박수박수박수?/다른 사람의 풀이.js
@@ -0,0 +1,7 @@
+function waterMelon(n) {
+  var result =
+    "수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박수박";
+  //함수를 완성하세요
+
+  return result.substring(0, n);
+}
