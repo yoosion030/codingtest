b77205fa7fec2456c7710399420fd4da6563bc9f
yoosion030 <rsy5487@naver.com>
2022-11-25 11:28:58 +0900

    가운데 글자 가져오기

---
+++ level1/가운데 글자 가져오기/다른 사람의 풀이.js
@@ -0,0 +1,8 @@
+function solution(s) {
+  return s.substr(Math.ceil(s.length / 2) - 1, s.length % 2 === 0 ? 2 : 1);
+}
+
+function solution(s) {
+  const mid = Math.floor(s.length / 2);
+  return s.length % 2 === 1 ? s[mid] : s[mid - 1] + s[mid];
+}
