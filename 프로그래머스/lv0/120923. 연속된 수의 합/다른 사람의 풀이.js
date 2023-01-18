2608ac026986d3cc26e1ce2d89403edcda8cf284
yoosion030 <rsy5487@naver.com>
2023-01-18 16:57:39 +0900

    연속된 수의 합

---
+++ 프로그래머스/lv0/120923. 연속된 수의 합/다른 사람의 풀이.js
@@ -0,0 +1,15 @@
+function solution(num, total) {
+  var min = Math.ceil(total / num - Math.floor(num / 2));
+  var max = Math.floor(total / num + Math.floor(num / 2));
+
+  return new Array(max - min + 1).fill(0).map((el, i) => {
+    return i + min;
+  });
+}
+
+function solution(num, total) {
+  const a = ((2 * total) / num + 1 - num) / 2;
+  return Array(num)
+    .fill()
+    .map((_, i) => i + a);
+}
