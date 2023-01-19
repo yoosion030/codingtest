518640572def2dd07f39fe0f1ac0d8ab398f8199
yoosion030 <rsy5487@naver.com>
2023-01-19 15:04:51 +0900

    영어가 싫어요

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/다른 사람의 풀이.js
@@ -0,0 +1,58 @@
+function solution(numbers) {
+  const obj = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+
+  const num = numbers.replace(
+    /zero|one|two|three|four|five|six|seven|eight|nine/g,
+    (v) => {
+      return obj[v];
+    }
+  );
+
+  return Number(num);
+}
+
+var solution = (n) =>
+  Number(
+    [
+      "zero",
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+    ].reduce((t, s, i) => t.replaceAll(s, i), n)
+  );
+
+function solution(numbers) {
+  const number = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+  for (let i = 0; i < number.length; i++) {
+    numbers = numbers.split(number[i]).join(i);
+  }
+  return +numbers;
+}
