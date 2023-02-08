b6a4868fc7cfc6e7f43b6ada3cf23f8082b333ec
yoosion030 <rsy5487@naver.com>
2023-02-08 20:43:15 +0900

    최댓값 만들기(2)

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/다른 사람의 풀이.js
@@ -0,0 +1,17 @@
+function solution(numbers) {
+  numbers.sort((a, b) => a - b);
+  return Math.max(
+    numbers[0] * numbers[1],
+    numbers[numbers.length - 1] * numbers[numbers.length - 2]
+  );
+}
+
+function solution(numbers) {
+  var answer = [];
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      answer.push(numbers[i] * numbers[j]);
+    }
+  }
+  return Math.max(...answer);
+}
