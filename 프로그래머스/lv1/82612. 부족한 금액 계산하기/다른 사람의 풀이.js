bcae916635c49fdeed3eefe1e2bb3f7fb1a18697
yoosion030 <rsy5487@naver.com>
2023-03-05 23:30:11 +0900

    부족한 금액 계산하기

---
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/다른 사람의 풀이.js
@@ -0,0 +1,16 @@
+function solution(price, money, count) {
+  const tmp = (price * count * (count + 1)) / 2 - money;
+  return tmp > 0 ? tmp : 0;
+}
+
+const solution = (..._) => Math.max((_[0] * _[2] * ++_[2]) / 2 - _[1], 0);
+
+function solution(price, money, count) {
+  let answer = 0;
+
+  for (let i = 1; i <= count; i++) {
+    answer += price * i;
+  }
+
+  return answer > money ? answer - money : 0;
+}
