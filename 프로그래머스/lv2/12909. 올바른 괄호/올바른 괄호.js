5adc25ce25f4c0a4fd68fc5ab059877754c016a9
유시온 <rsy5487@naver.com>
2023-04-27 15:07:02 +0900

    [level 2] Title: 올바른 괄호, Time: 6.18 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,13 @@
+function solution(s) {
+  let stack = [];
+  if (s[0] === ")" || s[s.length - 1] === "(") return false;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") stack.push(s[i]);
+    else if (s[i] === ")") {
+      if (stack.length === 0) return false;
+      stack.pop();
+    }
+  }
+
+  return stack.length > 0 ? false : true;
+}
