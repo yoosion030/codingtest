513eae5830d7f544f7f480077aee34f267fdcade
유시온 <rsy5487@naver.com>
2023-05-09 07:58:56 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,11 @@
+function solution(x) {
+  let sum = 0;
+
+  x.toString()
+    .split("")
+    .forEach((element) => {
+      sum += Number(element);
+    });
+
+  return x % sum === 0 ? true : false;
+}
