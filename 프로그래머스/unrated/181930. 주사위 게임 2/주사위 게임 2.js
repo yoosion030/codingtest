8e85f1d2851a2df8c15d724500101aeba84ee006
유시온 <rsy5487@naver.com>
2023-08-21 07:23:53 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,9 +1,9 @@
 function solution(a, b, c) {
-    if(a === b && b === c && c === a) {
-        return (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
-    }
-    if(a === b || b === c || a === c) {
-        return (a + b + c) * (a**2 + b**2 + c**2)
-    }
-    if(a !== b && b !== c && c !== a) return a + b + c
+  let sum1 = a + b + c;
+  let sum2 = a * a + b * b + c * c;
+  let sum3 = a ** 3 + b ** 3 + c ** 3;
+
+  if (a === b && b === c) return sum1 * sum2 * sum3;
+  else if (a === b || a === c || b === c) return sum1 * sum2;
+  else return sum1;
 }
