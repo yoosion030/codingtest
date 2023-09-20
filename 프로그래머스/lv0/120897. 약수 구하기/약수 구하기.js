40b894cb1635e5ce89e9ea4441106abebd3612a5
유시온 <rsy5487@naver.com>
2023-09-20 16:59:05 -0700

    [level 0] Title: 약수 구하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -1,9 +1,11 @@
 function solution(n) {
-  let answer = [];
-  for (let i = 1; i <= n; i++) {
+  let answer = [n];
+  for (let i = 1; i < n / 2; i++) {
     if (n % i === 0) {
-      answer.push(i);
+      answer.push(i, n / i);
+      answer.push(n / i);
     }
   }
-  return answer;
+
+  return answer.sort((a, b) => a - b).filter((v, i) => answer.indexOf(v) === i);
 }
