958b41f5dbb06a7580b80ef8946672b4be511727
yoosion030 <yoosioff@gmail.com>
2023-09-29 14:40:28 +0900

    프로세스

---
+++ 프로그래머스/lv2/42587. 프로세스/다른 사람의 풀이.js
@@ -0,0 +1,39 @@
+function solution(priorities, location) {
+  var list = priorities.map((t, i) => ({
+    my: i === location,
+    val: t,
+  }));
+  var count = 0;
+  while (true) {
+    var cur = list.splice(0, 1)[0];
+    if (list.some((t) => t.val > cur.val)) {
+      list.push(cur);
+    } else {
+      count++;
+      if (cur.my) return count;
+    }
+  }
+}
+
+function solution(priorities, location) {
+  var arr = priorities.map((priority, index) => {
+    return {
+      index: index,
+      priority: priority,
+    };
+  });
+
+  var queue = [];
+
+  while (arr.length > 0) {
+    var firstEle = arr.shift();
+    var hasHighPriority = arr.some((ele) => ele.priority > firstEle.priority);
+    if (hasHighPriority) {
+      arr.push(firstEle);
+    } else {
+      queue.push(firstEle);
+    }
+  }
+
+  return queue.findIndex((queueEle) => queueEle.index === location) + 1;
+}
