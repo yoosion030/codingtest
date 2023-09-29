203e06938642aefd9e5197cd412efecada38d62a
유시온 <rsy5487@naver.com>
2023-09-28 22:38:28 -0700

    [level 2] Title: 프로세스, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.js
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.js
@@ -1,18 +1,14 @@
 function solution(priorities, location) {
   let count = 0;
   while (priorities.length !== 0) {
-    // 우선순위 프로세스
     let max = Math.max(...priorities);
     // 1. 실행 대기 큐(Queue)에서 대기중인 프로세스 하나를 꺼냅니다.
     let pending = priorities.shift();
 
-    // 우선순위와 비교
-    if (pending < max) {
-      // 2. 큐에 대기중인 프로세스 중 우선순위가 더 높은 프로세스가 있다면 방금 꺼낸 프로세스를 다시 큐에 넣습니다.
+    // 2. 큐에 대기중인 프로세스 중 우선순위가 더 높은 프로세스가 있다면 방금 꺼낸 프로세스를 다시 큐에 넣습니다.
+    if (max > pending) {
       priorities.push(pending);
-      if (location === 0) {
-        location = priorities.length;
-      }
+      if (location === 0) location = priorities.length;
     } else {
       // 3. 만약 그런 프로세스가 없다면 방금 꺼낸 프로세스를 실행합니다.
       count++;
