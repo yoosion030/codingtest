f6ed3a1401a7525763be894538b3b5f9b94c5683
유시온 <rsy5487@naver.com>
2023-09-28 19:27:12 -0700

    [level 1] Title: 폰켓몬, Time: 3.72 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,4 +1,18 @@
 function solution(nums) {
-  const kind = [...new Set(nums)];
-  return nums.length / 2 < kind.length ? nums.length / 2 : kind.length;
+  const hashMap = {}; // 해시 맵 생성
+
+  // 해시 맵에 폰켓몬 종류 번호를 기록
+  for (const num of nums) {
+    if (!hashMap[num]) {
+      hashMap[num] = 1;
+    } else {
+      hashMap[num]++;
+    }
+  }
+
+  const uniquePokemonCount = Object.keys(hashMap).length; // 다양한 종류의 폰켓몬 개수
+  const maxSelectCount = nums.length / 2; // 선택해야 하는 최대 폰켓몬 개수
+
+  // 다양한 종류의 폰켓몬 개수와 선택해야 하는 최대 폰켓몬 개수 중 작은 값을 반환
+  return Math.min(uniquePokemonCount, maxSelectCount);
 }
