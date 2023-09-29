c1f447df74bc9480d02056768967f8d3e8165aec
유시온 <rsy5487@naver.com>
2023-09-28 20:43:40 -0700

    [level 1] Title: 같은 숫자는 싫어, Time: 43.04 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,9 +1,4 @@
-function solution(arr) {
-  let answer = [];
-
-  arr.map(
-    (value, index) => arr[index] !== arr[index + 1] && answer.push(value)
-  );
-
-  return answer;
+function solution(arr)
+{
+    return arr.filter((val,index) => val != arr[index+1]);
 }
