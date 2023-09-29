dfd5817903b6b788428c0f87a0653e561e1c59ea
유시온 <rsy5487@naver.com>
2023-09-28 19:37:58 -0700

    [level 1] Title: 완주하지 못한 선수, Time: 57.63 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,7 +1,6 @@
 function solution(participant, completion) {
   const hashMap = {}; // 해시 맵 생성
 
-  // 해시 맵에 폰켓몬 종류 번호를 기록
   for (const name of participant) {
     if (!hashMap[name]) {
       hashMap[name] = 1;
