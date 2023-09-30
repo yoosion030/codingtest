ca403f53e672f2cb810feaf6ee37a73bac90e38c
유시온 <rsy5487@naver.com>
2023-09-29 22:34:24 -0700

    [level 1] Title: K번째수, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,5 +1,12 @@
 function solution(array, commands) {
-  return commands.map(
-    ([i, j, k]) => array.slice(i - 1, j).sort((a, b) => a - b)[k - 1]
-  );
+    const result = [];
+
+    for (const command of commands) {
+        const [i, j, k] = command;
+        const slicedArray = array.slice(i - 1, j); // 인덱스는 0부터 시작하므로 i-1
+        slicedArray.sort((a, b) => a - b); // 오름차순 정렬
+        result.push(slicedArray[k - 1]); // k번째 원소를 결과 배열에 추가
+    }
+
+    return result;
 }
