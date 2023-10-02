7ff969018aa1069aaac735555724d3a6a1f0aa25
yoosion030 <yoosioff@gmail.com>
2023-10-02 17:13:22 +0900

    로그인 성공?

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/다른 사람의 풀이.js
@@ -0,0 +1,6 @@
+function solution(id_pw, db) {
+  const [id, pw] = id_pw;
+  const map = new Map(db);
+  console.log(map);
+  return map.has(id) ? (map.get(id) === pw ? "login" : "wrong pw") : "fail";
+}
