3011a872ab30e27766493653c1a4ed893153817c
유시온 <rsy5487@naver.com>
2023-11-06 01:02:30 -0800

    [level 1] Title: 약수의 합, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,13 +1,13 @@
 function solution(n) {
-  let sum = [n];
-  for (let i = 1; i <= n / 2; i++) {
+  let sum = 0;
+  for (let i = 1; i <= Math.sqrt(n); i++) {
     if (n % i === 0) {
-      sum.push(i);
-      if (!sum.includes(i)) {
-        sum.push(n / i);
+      sum += i;
+      if (i !== n / i) {
+        sum += n / i;
       }
     }
   }
 
-  return sum.reduce((a, b) => a + b);
+  return sum;
 }
